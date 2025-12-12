c2f222a8508b1a223c7d6113e416cfa1c51898ec
Poornashree <154966198+Poorna-shree@users.noreply.github.com>
2025-12-12 23:32:22 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ import React, { createContext } from "react";
 export const authDataContext = createContext();
 
 function AuthContext({ children }) {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://zerowastebite-backend.onrender.com";
 
   return (
     <authDataContext.Provider value={{ serverUrl }}>
